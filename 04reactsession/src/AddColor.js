3ca45d8e72b915aea451fbbe24c3c2ae11f688ed
Poonam <poonam.chauhan229@gmail.com>
2023-01-25 17:00:13 +0530

    session 4 completed

---
+++ 04reactsession/src/AddColor.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { ColorBox } from "./ColorBox";
+
+const AddColor = () => {
+  const [color, setColor] = useState("");
+  const inputstyle = {
+    backgroundColor: color
+  };
+
+  const initialcolorList = ["red", "orange", "yellow"];
+  const [colorList, setcolorList] = useState(initialcolorList);
+  return (
+    <div>
+      <input style={inputstyle} type="text"
+        value={color}
+        onChange={(e) => {
+          setColor(e.target.value);
+        }} />
+
+      <button onClick={() => {
+        setcolorList([...colorList, color]);
+      }}>Add Color</button>
+      {colorList.map((element) => {
+        return <ColorBox color={element} />;
+      })}
+    </div>
+  );
+};
+export default AddColor;
