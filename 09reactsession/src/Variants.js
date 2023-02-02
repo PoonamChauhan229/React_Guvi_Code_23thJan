bf0568b3e6c59d64919bd0a1471f0518302b26d1
Poonam <poonam.chauhan229@gmail.com>
2023-02-03 01:09:55 +0530

    movie app completed

---
+++ 09reactsession/src/Variants.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+export default function Variants() {
+  return (
+    <Box
+      
+    >
+      <Paper variant="outlined" />
+      <Paper variant="outlined" square />
+    </Box>
+  );
+}
