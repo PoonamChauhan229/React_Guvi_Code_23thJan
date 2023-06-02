63de6fb69dd00965ac5bdde827ec956065176eea
Poonam <poonam.chauhan229@gmail.com>
2023-06-02 23:03:44 +0530

    day 3 started

--- 03reactsession/03-02-reactsession-movieapp/src/Components/Movie.js
+++ 03reactsession/03-02-reactsession-movieapp/src/Components/Movie.js
@@ -2,14 +2,20 @@ import React from 'react'
 
 const Movie = ({name,poster,rating,summary}) => {
   console.log(name,poster,rating,summary)
+  const styles={
+    // Conditional Styling
+   color: rating>8?"green":"red"
+  }
   return (
     <div className='movieContainer'>
       <img src={poster}alt="" className="moviePoster" />
       {/* needs side by side so wrap it in a div */}
       <div className='movieSpecs'>
           <h4 className="movieName">{name}</h4>
-          <h4 className="movieRating">⭐{rating}</h4>
+          <h4 className="movieRating" style={styles}>⭐{rating}</h4>
       </div>
+      {/* Toggle Summary Task */}
+      <button>🔽</button>
       <p className="movieSummary">{summary}</p>
     </div>
   )
