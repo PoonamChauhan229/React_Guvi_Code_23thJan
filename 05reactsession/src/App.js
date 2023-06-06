e2793ddbb4706bb5c9f3a4368b1b34f382e6c72c
Poonam <poonam.chauhan229@gmail.com>
2023-06-06 18:15:08 +0530

    day 5 completed

--- 05reactsession/src/App.js
+++ 05reactsession/src/App.js
@@ -17,19 +17,23 @@ function App() {
   return (
     <>
       <Navbar/>
-      <AddMovie movieList={movieList} setMovieList={setMovieList}/>     
+      {/* <AddMovie movieList={movieList} setMovieList={setMovieList}/>      */}
       {/* <MovieList movieList={allmovies}/>  */}
       
-      <MovieList movieList={movieList}/>
-      {/* <Routes>
+      {/* <MovieList movieList={movieList}/> */}
+      <Routes>
           <Route exact path='/' element={<MovieList movieList={movieList}/> }/>
           <Route exact path='/addmovie' element={<AddMovie movieList={movieList} setMovieList={setMovieList} />}/>
           <Route exact path='/colorgame' element={<AddColor/>}/>
           <Route exact path="/addcolor" element={<Navigate replace to="/colorgame"/>}/>
           
+          {/* <Route path='*' element={<NotFound/>}/>  */}
+
+          {/* Redirection */}
+
           <Route path='/404' element={<NotFound/>}/> 
           <Route path="*" element={<Navigate replace to ="/404"/>}/>
-      </Routes>      */}
+      </Routes>     
       
     </>
   );
